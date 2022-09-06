ed1f2bc2aca627c69d902ec3689b186bfede7251
csnicolela.marcelo@gmail.com <csnicolela.marcelo@gmail.com>
2022-09-06 15:43:10 -0300

    Exercicos Aula 12 dia 06/09

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,4 @@
-const num = //seu numero aqui
-
-if(){
-    console.log("sua mensagem aqui");
+const num = 6;
+if (num % 2 === 0) {
+    console.log("o número é par");
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,8 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt('Escreva aqui sua nacionalidade').toLowerCase()
+
+if(nacionalidade === "brasileira"){console.log("Brasileira")}
+else if(nacionalidade === "argentina"){console.log("Argentina")}
+else if(nacionalidade === "uruguaia"){console.log("Uruguaia")}
+else if(nacionalidade === "chilena"){console.log("Chilena")}
+else if(nacionalidade === "colombiana"){console.log("Colombiana")}
+else{console.log(`nacionalidade ${nacionalidade} não encontrada`)}
